be52de5dd7063af729e8b56bc73d8d523332e88b
Alyssa Pratt <alyssakpratt@gmail.com>
2021-06-06 11:50:53 -0500

    creates form in app.js

--- set-times/src/App.js
+++ set-times/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import {Row, Col, Button, Form, Card} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Row>
+        <Col>
+          <h1>Search your area:</h1>
+        </Col>
+        </Row>
+        <Row>
+            {/* xs={4} takes the one third  of the page*/}
+            <Col xs={4} className="text-center">
+                <Card>
+                  <Form>
+                      <Form.Group controlId="formLat">
+                          <Form.Label>Latitude</Form.Label>
+                          <Form.Control type="text"
+                          />
+                          <Form.Text className="text-muted">
+                          (example : 36.7201600)
+                          </Form.Text>
+                      </Form.Group>
+                  </Form>
+                </Card>
+            </Col>
+
+            <Col xs={4} className="text-center">
+                <Card>
+                  <Form>
+                      <Form.Group controlId="formLong">
+                          <Form.Label>Longitude</Form.Label>
+                          <Form.Control type="text"
+                          />
+                          <Form.Text className="text-muted">
+                          (example : -4.4203400)
+                          </Form.Text>
+                      </Form.Group>
+                  </Form>
+                </Card>
+            </Col>
+
+            <Col>
+                <Button>Check Coordinates</Button>
+            </Col>
+    </Row>
+    <Row>
+      <Card>
+        <h1>Search Results</h1>
+      </Card>
+    </Row>
+    </>
   );
 }
 
